fb58448543659367cd1036c37b0924ed016c68ff
shotofjin119 <shotofjin119@kakao.com>
2024-06-22 10:29:29 +0000

    lkj

--- src/main/java/com/devdoc/backend/config/WebMvcConfig.java
+++ src/main/java/com/devdoc/backend/config/WebMvcConfig.java
@@ -15,7 +15,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 설정 적용
                 // 개발환경(localhost) & 배포환경(krampoline) - URL 주소 확인
-                .allowedOrigins("http://localhost:3000", "https://k93d5f2951587a.user-app.krampoline.com")
+                .allowedOrigins("http://localhost:3000", "https://k72db46c526eba.user-app.krampoline.com")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 허용할 HTTP 메서드 설정
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true) // 인증 정보 허용
