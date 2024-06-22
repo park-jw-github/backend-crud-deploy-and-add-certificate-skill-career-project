63f417734f8452fdd0761dcf000c5aaead63d583
shotofjin119 <shotofjin119@kakao.com>
2024-06-22 08:02:44 +0000

    ongongong

--- src/main/java/com/devdoc/backend/security/TokenProvider.java
+++ src/main/java/com/devdoc/backend/security/TokenProvider.java
@@ -1,5 +1,6 @@
 package com.devdoc.backend.security;
 
+import com.devdoc.backend.model.UserEntity;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
